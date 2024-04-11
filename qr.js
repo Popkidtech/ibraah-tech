3b99a8add44a28d20842dddf67b3b545a2ff77f4
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-11 19:15:43 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -56,13 +56,30 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: 'SIGMA-MD;;;' + b64data });
+				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: 'BLACKPANTHER;;;' + b64data });
 	
 				   let SIGMA_MD_TEXT = `
-*_Qr Code By Maher Zubair_*
-*_Made With 🤍_*
-	
-_Don't Forget To Give Star To My Repo_`
+*𝘽𝙇𝘼𝘾𝙆 𝙋𝘼𝙉𝙏𝙃𝙀𝙍 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀*
+*𝙄𝘽𝙍𝘼𝙃𝙄𝙈 𝙏𝙀𝘾𝙃*
+*𝙊𝙒𝙉𝙀𝙍 𝙄𝘽𝙍𝘼𝙃𝙄𝙈 𝘼𝘿𝘼𝙈𝙎*
+____________________________________
+╔════◇
+║『 𝘽𝙇𝘼𝘾𝙆 𝙋𝘼𝙉𝙏𝙃𝙀𝙍 𝙏𝙃𝙀 𝙊𝙍𝙄𝙂𝙄𝙉𝘼𝙇 𝘽𝙊𝙏 』
+║ OK YOUR SESSION IS READY COPY IT  
+║ AND HOST IT ON HEROKU.
+╚════════════════════╝
+╔═════◇
+║ 『••• OWNER INFO •••』
+║❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _https://www.youtube.com/@ibrahimmdgpt_
+║❒ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/message/74F2PC4JA4F3P1_
+║❒ 𝐑𝐞𝐩𝐨: _https://github.com/ibrahimaitech/IBRAHIM-MD_
+║❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://chat.whatsapp.com/H6KaBzAhRnw52DM93KsRy2_
+║❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y_
+║ 🐯🐯🐯
+╚════════════════════╝ 
+ *©𝗜𝗕𝗥𝗔𝗛𝗜𝗠 𝗧𝗘𝗖𝗛*
+___________________________________
+*Yoo Don't forget to star the repo*_
 	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
 
 
